522e24a0445a3c68c2048f4afc504acb2770589a
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:28:19 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/* Tablas de Multiplicación
+• Diseñar un algoritmo que muestre por pantalla la tabla de
+multiplicación del número ingresado por el usuario
+• Para definir hasta qué número desea que muestre la tabla de
+multiplicación, el usuario también deberá ingresar dicho valo */
+
 let numeroIngresado: number = Number(
   prompt(`Ingrese Valor para conocer su tabla:`)
 );
